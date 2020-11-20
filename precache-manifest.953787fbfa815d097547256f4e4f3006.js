4ae8013347d930a40da42da03ea6d2fd5c6e7c87
AsemBasheer <eng.asem.basheer@gmail.com>
2020-11-20 20:31:51 +0200

    Updates

---
+++ precache-manifest.953787fbfa815d097547256f4e4f3006.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc6ca16d68041be7f942c0828b52db61",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1f6dc2fd90c647f1948d",
+    "url": "/portfolio/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "305623fc5ba4dabb6b46",
+    "url": "/portfolio/static/js/2.d940a925.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/portfolio/static/js/2.d940a925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f6dc2fd90c647f1948d",
+    "url": "/portfolio/static/js/main.788525cc.chunk.js"
+  },
+  {
+    "revision": "c3fe483896071cc46d75",
+    "url": "/portfolio/static/js/runtime-main.75c69042.js"
+  },
+  {
+    "revision": "24e877c8fd845e8ee6a68b77908a3df8",
+    "url": "/portfolio/static/media/asembasheer.24e877c8.png"
+  },
+  {
+    "revision": "1b9c7d37b95f7795cc31b02ba4357815",
+    "url": "/portfolio/static/media/resume.1b9c7d37.pdf"
+  }
+]);
