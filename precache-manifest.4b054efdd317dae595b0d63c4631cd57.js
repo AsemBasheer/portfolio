1a9a4f90e4a613a08a5fae3e04ecbc425aa3df97
AsemBasheer <AsemBasheer@users.noreply.github.com>
2021-01-10 18:58:06 +0200

    Updates

---
+++ precache-manifest.4b054efdd317dae595b0d63c4631cd57.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "518d2e6017fb98a9baf42ec46953c6cb",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bbd73ebfa29760b58bda",
+    "url": "/portfolio/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "1e0ac43e8f2d422b5623",
+    "url": "/portfolio/static/js/2.4568b8bd.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/portfolio/static/js/2.4568b8bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbd73ebfa29760b58bda",
+    "url": "/portfolio/static/js/main.c3352bbd.chunk.js"
+  },
+  {
+    "revision": "c3fe483896071cc46d75",
+    "url": "/portfolio/static/js/runtime-main.75c69042.js"
+  },
+  {
+    "revision": "24e877c8fd845e8ee6a68b77908a3df8",
+    "url": "/portfolio/static/media/asembasheer.24e877c8.png"
+  },
+  {
+    "revision": "86c5755864aebdf0b5122ed01b1daf81",
+    "url": "/portfolio/static/media/resume.86c57558.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.fccdc6dfe06362d6db15c3f290461bce.js"
+  "/portfolio/precache-manifest.4b054efdd317dae595b0d63c4631cd57.js"
 );
 
 self.addEventListener('message', (event) => {
